6ded761225c75c29daf55d2f6785040191d4f376
Yilmar Ospina <ospina.yilmar@gmail.com>
2023-12-13 20:17:32 -0500

    First example implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+
+const getRandomNumberByUrl = async (): Promise<number> => {
+  const res = await fetch(
+    'https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new'
+  );
+
+  const numberString = await res.text();
+  return +numberString;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [randomNumber, setRandomNumber] = useState(0);
+
+  useEffect(() => {
+    getRandomNumberByUrl().then((number) => setRandomNumber(number));
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setRandomNumber((count) => count + 1)}>
+          Random number is {randomNumber}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
