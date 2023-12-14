8f8bd82fa4acdcde6f01729724ff54b600c62c1e
Yilmar Ospina <ospina.yilmar@gmail.com>
2023-12-13 21:35:10 -0500

    Usabilidad implementada por medio de estados y efectos

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import './App.css';
 
 const getRandomNumberByUrl = async (): Promise<number> => {
@@ -6,24 +6,46 @@ const getRandomNumberByUrl = async (): Promise<number> => {
     'https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new'
   );
 
+  //throw new Error('Auxilio');
+
   const numberString = await res.text();
   return +numberString;
 };
 
 function App() {
-  const [randomNumber, setRandomNumber] = useState(0);
+  const [randomNumber, setRandomNumber] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>();
+  const [key, forceRefetch] = useReducer((x) => x + 1, 0);
 
   useEffect(() => {
-    getRandomNumberByUrl().then((number) => setRandomNumber(number));
-  }, []);
+    setIsLoading(true);
+    getRandomNumberByUrl()
+      .then(setRandomNumber)
+      .catch((error) => setError(error.message));
+  }, [key]);
+
+  useEffect(() => {
+    if (randomNumber) setIsLoading(false);
+  }, [randomNumber]);
+
+  useEffect(() => {
+    if (error) setIsLoading(false);
+  }, [error]);
 
   return (
     <>
       <div className="card">
-        <button onClick={() => setRandomNumber((count) => count + 1)}>
-          Random number is {randomNumber}
-        </button>
+        {isLoading ? (
+          <h2>Cargando...</h2>
+        ) : (
+          !error && <h2> Random number is {randomNumber}</h2>
+        )}
+        {!isLoading && error && <h3>{error}</h3>}
       </div>
+      <button onClick={forceRefetch} disabled={isLoading}>
+        {isLoading ? '...' : 'New number'}
+      </button>
     </>
   );
 }
